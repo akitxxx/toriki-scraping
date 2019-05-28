385f425b393da5ca0d47bcdff2c1c90ec609653f
aki <>
2019-05-28 23:07:57 +0900

    [up]fix error.

--- scraping.py
+++ scraping.py
@@ -2,14 +2,15 @@
 
 
 from selenium import webdriver
-import pandas
+import pandas as pd
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-
+ 
 options = Options()
 browser = webdriver.Chrome(
     '/usr/local/Caskroom/chromedriver/74.0.3729.6/chromedriver')
-# df = pandas.read_csv('default.csv', index_col=0)
+
+# data frame
 url = "https://www.torikizoku.co.jp/menu/"
 
 # メニューリスト
@@ -20,31 +21,28 @@ menu_link = 'a.left-arrow'
 
 browser.get(url)
 
-# メニューリンクリストを取得
-menu_link_list = browser.find_elements_by_css_selector(menu_link)
-
 # スクレイピング処理
+def scraping(menu_link_list, menu_name):
+    for menu in menu_link_list:
+        if menu.text == menu_name:
+            menu.click()
+            # title = browser.find_element_by_css_selector(
+            #     'h4.yellowLineTitle').text
+            # print(title)
 
+            item_list = browser.find_elements_by_css_selector('li h5')
 
-def scraping(menu, menu_name):
-    if menu.text == menu_name:
-        sleep(3)
-        menu.click()
-        sleep(3)
+            for item in item_list:
+                print(item.text)
 
-        title = browser.find_element_by_css_selector(
-            'h4.yellowLineTitle').text
-        print(title)
-
-        item_list = browser.find_elements_by_css_selector('li h5')
-
-        for item in item_list:
-            print(item.text)
-
+            print('\n')
+            browser.get(url)
+            break
 
 # 取得したいメニュー名と一致するメニューリンクにジャンプし、スクレイピング
 for menu_name in MENU_NAME_LIST:
-    for menu in menu_link_list:
-        scraping(menu, menu_name)
-
+    # メニューリンクリストを取得
+    menu_link_list = browser.find_elements_by_css_selector(menu_link)
+    scraping(menu_link_list, menu_name)
+ 
 browser.close()
